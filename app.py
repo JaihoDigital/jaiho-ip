545cdf01bd6c10523e0c63fcd6a88d979d351e43
avarshvir <avarshvir@gmail.com>
2025-12-17 16:23:49 +0530

    API docs and implementation

--- app.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get('https://jaiho-ip.vercel.app/api/json')
-data = response.json()
-
-print(f"IP Address: {data['ip']}")
-print(f"Country: {data['location']['country']}")
